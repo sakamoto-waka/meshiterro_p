71e9b188c39cc079591dc3ff6a8ddf995e352c85
sakamoto-waka <wakanmhl@gmail.com>
2022-02-14 05:47:20 +0000

    [finish]2-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  # ストロングパラメターみたいなものを定義
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+  end  
+  
 end
