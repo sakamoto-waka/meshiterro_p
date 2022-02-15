415e2f92068a58cdffad1328f9dfe8d7d63d8ab5
sakamoto-waka <wakanmhl@gmail.com>
2022-02-15 21:58:44 +0000

    [update] index desc

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,9 +1,9 @@
 class PostImagesController < ApplicationController
-  
+
   def new
     @post_image = PostImage.new
   end
-  
+
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
@@ -11,28 +11,28 @@ class PostImagesController < ApplicationController
       redirect_to post_images_path
     else
       render :new
-    end  
-  end  
+    end
+  end
 
   def index
-    @post_images = PostImage.page(params[:page])
+    @post_images = PostImage.page(params[:page]).all.order(created_at: :desc)
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     @post_image = PostImage.find(params[:id])
     @post_image.destroy
     redirect_to post_images_path
-  end  
-  
-  private
-  
-  def post_image_params
-    params.require(:post_image).permit(:shop_name, :image, :caption)  
   end
-  
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 end
